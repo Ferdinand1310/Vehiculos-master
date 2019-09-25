f5068cc7290e5c0bcc50d1d3c876be7e84dd36df
Ferdinand1310 <52927754+Ferdinand1310@users.noreply.github.com>
2019-09-24 22:18:31 -0600

    Initial commit

---
+++ MiVehiculo/MiVehiculo.Web/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MiVehiculo.BL;
+
+
+namespace MiVehiculo.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            var productosBL = new ProductosBL();
+            var listadeproductos = productosBL.ObtenerProductosActivos();
+
+          ViewBag.adminwebSiteUrl = ConfigurationManager.AppSettings["adminwebSiteUrl"];
+
+            return View(listadeproductos);
+
+        }
+    }
+}
